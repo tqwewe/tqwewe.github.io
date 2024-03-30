41e93736bb842f95162ca93bd5a695e343e9540c
Ari Seyhun <ariseyhun@live.com.au>
2024-03-30 13:39:47 +0800

    feat: add comments section

--- themes/adidoks/static/js/main.js
+++ themes/adidoks/static/js/main.js
@@ -1,19 +1,14 @@
 // Set darkmode
 document.getElementById('mode').addEventListener('click', () => {
   document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+  let theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+  if (window.CUSDIS) {
+    window.CUSDIS.setTheme(theme);
+  }
 });
   
 // Enforce local storage setting but also fallback to user-agent preferences
 if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
   document.body.classList.add('dark');
 }
-
-// Random status on homepage
-// const options = [
-//   'Lazy but efficient ☕',
-//   'Diving deep into Rust 🦀',
-// ]
-
-// const taglineEl = document.getElementById('tagline');
-// taglineEl.textContent = options[Math.floor(Math.random()*options.length)];
