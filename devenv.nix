1e7e6cb55ca6cd89c76a3b737f28d1c106554acd
Ari Seyhun <dev@tqwewe.com>
2026-09-08 17:56:08 +1000

    feat: upgrade zola to 0.23.4 and migrate theme to tera 2 components

--- devenv.nix
+++ devenv.nix
@@ -3,9 +3,29 @@
   ...
 }:
 
+let
+  # nixpkgs ships 0.22.1, but the theme templates are Tera 2, which needs Zola 0.23+.
+  zola = pkgs.stdenv.mkDerivation rec {
+    pname = "zola";
+    version = "0.23.4";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/getzola/zola/releases/download/v${version}/zola-v${version}-x86_64-unknown-linux-gnu.tar.gz";
+      hash = "sha256-VNGjR3gbLzIzCRT8wC3vgcfj3bYRGzbRzInAZVeu0d4=";
+    };
+
+    sourceRoot = ".";
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
+
+    installPhase = ''
+      install -Dm755 zola $out/bin/zola
+    '';
+  };
+in
 {
-  packages = with pkgs; [
-    git
+  packages = [
+    pkgs.git
     zola
   ];
 }
